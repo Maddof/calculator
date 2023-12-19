28dc5831e86d88060e1dd096f54101a67ee99e13
Maddof <97884486+Maddof@users.noreply.github.com>
2023-12-19 14:54:21 +0100

    Basic HTML, CSS, JavaScript

---
+++ main.js
@@ -0,0 +1,28 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const division = function (a, b) {
+  return a / b;
+};
+
+let num1;
+let operator;
+let num2;
+
+const operate = function (num1, num2, operator) {
+  add(num1, num2);
+};
+
+console.log(division(1, 2));
+console.log(multiply(5, 3));
+console.log(subtract(10, 3));
+console.log(add(5, 3));
