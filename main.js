c99174a95be514fe99dcade194f8ed87b99760b4
Maddof <97884486+Maddof@users.noreply.github.com>
2023-12-19 16:49:40 +0100

    Created helper functions and eventlistener to display numbers on screen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = function (a, b) {
+const add = (a, b) => {
   return a + b;
 };
 
@@ -19,10 +19,46 @@ let operator;
 let num2;
 
 const operate = function (num1, num2, operator) {
-  add(num1, num2);
+  switch (operator) {
+    case "+":
+      console.log(add(num1, num2));
+      break;
+    case "-":
+      console.log(subtract(num1, num2));
+      break;
+    case "÷":
+      console.log(division(num1, num2));
+      break;
+    case "x":
+      console.log(multiply(num1, num2));
+      break;
+  }
 };
 
-console.log(division(1, 2));
-console.log(multiply(5, 3));
-console.log(subtract(10, 3));
-console.log(add(5, 3));
+operate(1, 5, "÷");
+
+// let numberButton = document.querySelectorAll(".number");
+// console.log(numberButton);
+let buttons = document.querySelector(".buttons");
+let display = document.querySelector(".screen");
+
+buttons.addEventListener("click", (e) => {
+  // If the button clicked on has the classes of btn & number
+  // we call appendNumber with its current content
+  if (e.target.className === "btn number") {
+    appendNumber(e.target.textContent);
+  }
+});
+
+//Helper functions
+
+function appendNumber(number) {
+  if (display.textContent === "0") {
+    resetDisplay();
+  }
+  display.textContent += number;
+}
+
+function resetDisplay() {
+  display.textContent = "";
+}
